7f9766fe0dedd3ee657cd15be729bcbee298d835
zh920126 <53657400+zh920126@users.noreply.github.com>
2019-11-27 20:49:51 +0800

    登录页面基本样式完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入样式文件
+import '@/styles/index.css'
 // 全局引入element-ui框架
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="login">
-    <el-form :model="userinfo" :rules="rules" ref="userinfo" label-width="100px" class="demo-ruleForm">
+   <div class="container">
+    <div class="avatar">
+       <img src="@/assets/avatar.jpg" alt="">
+    </div>
+      <el-form :model="userinfo" :rules="rules" ref="userinfo" label-width="100px" class="demo-ruleForm">
   <el-form-item label="用户名" prop="username">
     <el-input v-model="userinfo.username"></el-input>
   </el-form-item>
@@ -8,10 +12,10 @@
     <el-input v-model="userinfo.password" type='password'></el-input>
   </el-form-item>
   <el-form-item>
-    <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-    <el-button @click="resetForm('ruleForm')">重置</el-button>
+    <el-button type="primary" class="login-btn">登录</el-button>
   </el-form-item>
 </el-form>
+   </div>
   </div>
 </template>
 
@@ -38,6 +42,36 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.login {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: #2f4050;
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 0px 40px 15px 40px;
+    margin: 200px auto;
+    background: white;
+    .avatar {
+      position: relative;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+      margin-left: -60px;
+      margin-top: -60px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-shadow: 0 1px 5px #ccc;
+      overflow: hidden;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
 </style>
