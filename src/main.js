e679085673c749c74b0f457aa89ec62388193260
zh920126 <53657400+zh920126@users.noreply.github.com>
2019-11-27 20:40:22 +0800

    使用element框架，生成登录框

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 全局引入element-ui框架
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login">
+    <el-form :model="userinfo" :rules="rules" ref="userinfo" label-width="100px" class="demo-ruleForm">
+  <el-form-item label="用户名" prop="username">
+    <el-input v-model="userinfo.username"></el-input>
+  </el-form-item>
+  <el-form-item label="密码" prop="password">
+    <el-input v-model="userinfo.password" type='password'></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+    <el-button @click="resetForm('ruleForm')">重置</el-button>
+  </el-form-item>
+</el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      userinfo: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, message: '用户名长度不小于3个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 3, max: 16, message: '密码长度为3-16位', trigger: 'blur' }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
